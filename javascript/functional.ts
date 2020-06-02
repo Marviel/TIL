e6369b34366ad153d00dbf1427950878a3bbceba
Luke Bechtel <lukebechtel4@gmail.com>
2020-06-02 10:17:59 -0400

    Add `objectKeyedOnArrayProperty`

--- javascript/functional.ts
+++ javascript/functional.ts
@@ -9,3 +9,11 @@ export function switchReturn<S, T>(value: S, mapping: Array<[(val: S) => boolean
     // If we don't find anything, the last case is our default.
     return defaultCase;
 }
+
+// Gets an object which contains the elements of the given array, keyed on a property given by a selector function.
+export function objectKeyedOnArrayProperty<T>(arr: T[] , propertyGetter: (inputObj: T) => number): {[key: number]: T} {
+    return arr.reduce((acc: any, item) => {
+        acc[propertyGetter(item)] = item;
+        return acc;
+    }, {});
+}
